7be8ef497380ed5034101f8f3f876c2f065ee439
Kowalski1024 <tomasz_wojnar@hotmail.com>
2024-01-03 00:23:32 +0100

    Refactor Passage class and add new subclasses

--- SC2MapSegmentation/dataclasses/passage.py
+++ SC2MapSegmentation/dataclasses/passage.py
@@ -1,92 +1,75 @@
-from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, NamedTuple
+
+import numpy as np
 
 from sc2.client import Client
 from sc2.game_info import GameInfo
 from sc2.position import Point2, Point3
 
-from SC2MapSegmentation.utils import group_points
+import SC2MapSegmentation.utils.debug_utils as debug
+from SC2MapSegmentation.utils.misc_utils import get_terrain_z_height
 
 
-@dataclass(frozen=True)
-class Passage:
-    game_info: GameInfo
-    vision_blockers: Optional[frozenset[Point2]]
-    destructables: Optional[set[Point2]]
-    minerals: Optional[set[Point2]]
+class Passage(NamedTuple):
+    surrounding_tiles: frozenset[Point2]
+    tiles: Optional[frozenset[Point2]] = None
 
-    titles: frozenset[Point2]
-    surrounding: frozenset[Point2]
-    connections: dict[int, tuple[Point2, ...]] = field(default_factory=dict)
+    vision_blockers: Optional[frozenset[Point2]] = None
+    destructables: Optional[set[int]] = None
+    minerals: Optional[set[int]] = None
+
+    connections: dict[int, tuple[Point2, ...]] = dict()
 
     def center(self) -> Point2:
-        if self.titles:
-            return Point2.center(self.titles)
-        else:
-            return Point2.center(self.surrounding)
+        points = self.tiles if self.tiles else self.surrounding_tiles
+        return Point2.center(points)
+    
+    def tiles_indices(self) -> tuple[np.array, np.array]:
+        x, y = zip(*self.tiles)
+        return np.array(x), np.array(y)
+    
+    def __hash__(self) -> int:
+        return hash(self.tiles)
 
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"center={self.center}, "
+            f"surrounding_length={len(self.surrounding_tiles)}, "
+            f"tiles_length={len(self.tiles)}, "
+            f"vision_blockers_length={len(self.vision_blockers)}, "
+            f"destructables_length={len(self.destructables)}, "
+            f"minerals_length={len(self.minerals)}, "
+            f"connections_keys={self.connections.keys()})"
+        )
+    
+    def draw_boxes(self, game_info: GameInfo, client: Client, height_offset: int = -0.15):
+        for point in self.tiles:
+            debug.draw_point(game_info, client, point, height_offset + 0.1, debug.RED)
 
-    def draw_boxes(self, client: Client, height_offset: int = -0.1):
-        for point in self.titles:
-            height = self._get_terrain_z_height(point)
-
-            client.debug_text_world(f"{height:.2f}", Point3((point.x + 0.5, point.y + 0.5, height)), size=10)
-            point = Point3((point.x + 0.5, point.y + 0.5, height + height_offset))
-            client.debug_box2_out(point, color=(0, 255, 0))
-
-        self.draw_surrounding(client, height_offset)
+        self.draw_surrounding_regions(game_info, client, height_offset)
 
         center = self.center()
-        center = Point3((center.x, center.y, self._get_terrain_z_height(center) + height_offset))
-        client.debug_sphere_out(center, r=1, color=(255, 255, 255))
+        center = Point3((center.x, center.y, get_terrain_z_height(game_info, center)))
+        client.debug_sphere_out(center, r=1, color=debug.WHITE)
 
-    def draw_surrounding(self, client: Client, height_offset: int = -0.15):
-        for region, points in self.connections.items():
-            for point in points:
-                height = self._get_terrain_z_height(point)
-
-                client.debug_text_world(f"{region}", Point3((point.x + 0.5, point.y + 0.5, height)), size=10)
-                point = Point3((point.x + 0.5, point.y + 0.5, height + height_offset))
-                client.debug_box2_out(point, color=(255, 0, 0))
-
-    def _get_terrain_z_height(self, pos: Point2) -> float:
-        pos = pos.rounded
-        return -16 + 32 * self.game_info.terrain_height[pos] / 255
+    def draw_surrounding_regions(self, game_info: GameInfo, client: Client, height_offset: int = -0.15):
+        if self.connections:
+            for region, points in self.connections.items():
+                for point in points:
+                    debug.draw_point(game_info, client, point, height_offset, debug.GREEN, f"{region}")
+        else:
+            for point in self.surrounding_tiles:
+                debug.draw_point(game_info, client, point, height_offset, debug.GREEN)
     
-    def __hash__(self):
-        return hash(self.titles)
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}(points_len={len(self.titles)}, surrounding_len={len(self.surrounding)}, destructables={self.destructables}, minerals={self.minerals}), vision_blockers={self.vision_blockers})"
-
-
-@dataclass(frozen=True)
 class Ramp(Passage):
-    low_tiles: tuple[Point2, ...] = field(default_factory=tuple)
-    high_tiles: tuple[Point2, ...] = field(default_factory=tuple)
-
-    # def draw_surrounding(self, client: Client, height_offset: int = -0.1):
-    #     for point in self.low_tiles:
-    #         height = self._get_terrain_z_height(point)
-    #         x = self.game_info.terrain_height[point]
-
-    #         client.debug_text_world(f"{x:.2f}", Point3((point.x + 0.5, point.y + 0.5, height)), size=10)
-    #         point = Point3((point.x + 0.5, point.y + 0.5, height + height_offset))
-    #         client.debug_box2_out(point, color=(255, 0, 0))
-
-    #     for point in self.high_tiles:
-    #         height = self._get_terrain_z_height(point)
-    #         x = self.game_info.terrain_height[point]
-
-    #         client.debug_text_world(f"{x:.2f}", Point3((point.x + 0.5, point.y + 0.5, height)), size=10)
-    #         point = Point3((point.x + 0.5, point.y + 0.5, height + height_offset))
-    #         client.debug_box2_out(point, color=(0, 0, 255))
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(points_len={len(self.titles)}, surrounding_len={len(self.surrounding)}, destructables={self.destructables}, minerals={self.minerals}), vision_blockers={self.vision_blockers})"
+    pass
 
 
-@dataclass(frozen=True)
 class ChokePoint(Passage):
-    def __repr__(self):
-        return f"{self.__class__.__name__}(points_len={len(self.titles)}, surrounding_len={len(self.surrounding)}, destructables={self.destructables}, minerals={self.minerals}), vision_blockers={self.vision_blockers})"
+    pass
+
+
+class Cliff(Passage):
+    pass
--- SC2MapSegmentation/passages.py
+++ SC2MapSegmentation/passages.py
@@ -2,106 +2,55 @@ from typing import Iterable, Any, Mapping, Type, Union
 import random
 import numpy as np
 from collections import Counter, defaultdict
-from itertools import chain
 
 from sc2.game_info import GameInfo
 from sc2.position import Point2
 from sc2.unit import Unit
 from .dataclasses.passage import Passage, Ramp, ChokePoint
-from .utils import unbuildable_pathing_grid, flood_fill_all, find_surrounding, group_points, flood_fill_points, flood_fill
 
-import matplotlib.pyplot as plt
-
-
-GROUND_HEIGHT = (175, 191, 207)
-
-
-def create_edge(points: Iterable[Point2]) -> tuple[Point2, ...]:
-    """
-    Creates an edge of a passage, e.g. the low ground tiles of a ramp.
-
-    Args:
-        points (Iterable[Point2]): points to create the edge from
-
-    Returns:
-        tuple[Point2, ...]: edge points
-    """
-    center = Point2.center(points)  # type: ignore
-    furthest = max(points, key=lambda p: p.distance_to(center))
-    return tuple(flood_fill_points(furthest, points))
+from .utils.misc_utils import GROUND_HEIGHT, mark_unbuildable_tiles
+from .utils.algorithms import group_connected_points, flood_fill_all, find_surrounding, flood_fill_all
+from .utils.data_structures import Point
 
 
 def passage_info(
         game_info: GameInfo,
-        titles: Iterable[Point2],
+        tiles: Iterable[Point],
+        surrounding_tiles: Iterable[Point],
         destructables: Iterable[Unit],
         minerals: Iterable[Unit],
 ) -> Mapping[str, Any]:
-    """
-    Creates a dictionary with information about a passage.
-
-    Args:
-        game_info (GameInfo): game info
-        titles (Iterable[Point2]): tiles of the passage
-        destructables (Iterable[Unit]): destructables in the passage
-        minerals (Iterable[Unit]): minerals in the passage
-
-    Returns:
-        Mapping[str, Any]: passage information
-    """
+    surrounding_tiles = frozenset(map(Point2, surrounding_tiles))
+    tiles = frozenset(map(Point2, tiles))
 
     return {
-        "game_info": game_info,
-        "titles": frozenset(titles),
-        "surrounding": frozenset(find_surrounding(titles, game_info.placement_grid.data_numpy, lambda x: x == 1)),
-        "vision_blockers": frozenset(p for p in game_info.vision_blockers if p in titles),
-        "minerals": {m.tag for m in minerals if m.position.rounded in titles},
-        "destructables": {d.tag for d in destructables if d.position.rounded in titles}
+        "tiles": tiles,
+        "surrounding_tiles": surrounding_tiles,
+        "vision_blockers": frozenset(p for p in game_info.vision_blockers if p in tiles),
+        "minerals": {m.tag for m in minerals if m.position.rounded in tiles},
+        "destructables": {d.tag for d in destructables if d.position.rounded in tiles}
     }
 
 
 def create_passage(
         passage_type: Type[Passage],
         game_info: GameInfo,
-        tiles: Iterable[Point2],
+        tiles: Iterable[Point],
         destructables: Iterable[Unit],
         minerals: Iterable[Unit]
 ) -> Passage:
-    """
-    Creates a passage.
-
-    Args:
-        passage_type (Type[Passage]): type of the passage
-        game_info (GameInfo): game info
-        tiles (Iterable[Point2]): tiles of the passage
-        destructables (Iterable[Unit]): destructables in the passage
-        minerals (Iterable[Unit]): minerals in the passage
-
-    Returns:
-        Passage: created passage
-    """
-
-    info = passage_info(game_info, tiles, destructables, minerals)
+    grid = game_info.placement_grid.data_numpy.T
+    surrounding_tiles = find_surrounding(tiles, lambda p: grid[p.x, p.y] == 1)
+    info = passage_info(game_info, tiles, surrounding_tiles, destructables, minerals)
 
     if passage_type == Ramp:
-        groups = group_points(info["surrounding"])
+        groups = group_connected_points(surrounding_tiles)
 
         if len(groups) != 2:
             raise ValueError(f"Expected 2 groups, got {len(groups)}.")
 
-        low_tiles = create_edge(groups[0])
-        high_tiles = create_edge(groups[1])
-
-        # make sure the high ground is actually higher than the low ground
-        low_center = Point2.center(low_tiles).rounded  # type: ignore
-        high_center = Point2.center(high_tiles).rounded  # type: ignore
-        if game_info.terrain_height[high_center] < game_info.terrain_height[low_center]:
-            high_tiles, low_tiles = low_tiles, high_tiles
-
         return Ramp(
             **info,
-            high_tiles=high_tiles,
-            low_tiles=low_tiles
         )
     else:
         return ChokePoint(**info)
@@ -122,7 +71,7 @@ def find_region_passages(
         surrounding = find_surrounding(region, region_grid, lambda val: val > 0, neighbors4=True)
         connections_set.update(surrounding)
 
-    for group in group_points(connections_set, True):
+    for group in group_connected_points(connections_set, True):
         tiles_per_region = Counter((region_grid[p.y, p.x] for p in group))
         if len(tiles_per_region) > 1:
             passages.append(Passage(
@@ -131,7 +80,7 @@ def find_region_passages(
                 destructables=None,
                 minerals=None,
                 titles=frozenset(),
-                surrounding=frozenset(group),
+                surrounding_tiles=frozenset(group),
             ))
 
     return passages
@@ -141,11 +90,11 @@ def clean_and_update(passages: Iterable[Passage], grid: np.ndarray):
     results = []
 
     for passage in passages:
-        if len(passage.surrounding) == 0:
+        if len(passage.surrounding_tiles) == 0:
             continue
 
         connections = defaultdict(list)
-        for point in passage.surrounding:
+        for point in passage.surrounding_tiles:
             val = grid[point.y, point.x]
             if val == 0:
                 continue
@@ -167,32 +116,19 @@ def find_passages(
         minerals: Iterable[Unit],
         threshold: int = 4
 ) -> list[Passage]:
-    """
-    Finds all passages in the map.
-
-    Args:
-        game_info (GameInfo): game info
-        destructables (Iterable[Unit]): destructables in the map
-        minerals (Iterable[Unit]): minerals in the map
-        threshold (int, optional): minimum size of passages
-
-    Returns:
-        list[Passage]: found passages
-    """
-
-    pathing_grid = game_info.pathing_grid.data_numpy
-    placement_grid = game_info.placement_grid.data_numpy
-    height_grid = game_info.terrain_height.data_numpy
+    pathing_grid = game_info.pathing_grid.data_numpy.T
+    placement_grid = game_info.placement_grid.data_numpy.T
+    height_grid = game_info.terrain_height.data_numpy.T
     vision_blockers = game_info.vision_blockers
 
-    grid = unbuildable_pathing_grid(pathing_grid, placement_grid, destructables, minerals,
-                                    vision_blockers)
+    grid = mark_unbuildable_tiles(pathing_grid, placement_grid, destructables, minerals, vision_blockers)
 
     # find all ramps
-    ramp_tiles = flood_fill_all(grid, lambda p: grid[p.y, p.x] == 1 and height_grid[p.y, p.x] not in GROUND_HEIGHT)
+    ramp_tiles = flood_fill_all(grid.shape, lambda p: grid[p.x, p.y] == 1 and height_grid[p.x, p.y] not in GROUND_HEIGHT)
     ramp_tiles = [tiles for tiles in ramp_tiles if len(tiles) >= threshold]
+
     for tiles in ramp_tiles:
-        tiles.update(find_surrounding(tiles, grid, lambda x: x == 1))
+        tiles.update(find_surrounding(tiles, lambda p: grid[p.x, p.y] == 1))
 
     ramps = [
         create_passage(Ramp, game_info, tiles, destructables, minerals)
@@ -201,11 +137,10 @@ def find_passages(
 
     # remove ramps from grid
     for ramp in ramps:
-        for tile in ramp.titles:
-            grid[tile.y, tile.x] = 0
+        grid[ramp.tiles_indices()] = 0
 
     # find all chokes
-    choke_tiles = flood_fill_all(grid, lambda p: grid[p.y, p.x] == 1 and height_grid[p.y, p.x] in GROUND_HEIGHT)
+    choke_tiles = flood_fill_all(grid.shape, lambda p: grid[p.x, p.y] == 1 and height_grid[p.x, p.y] in GROUND_HEIGHT)
 
     chokes = [
         create_passage(ChokePoint, game_info, tiles, destructables, minerals)
