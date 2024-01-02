a0c8de8c5c8bd9c54166259fe638ccdb5da9eb93
Kowalski1024 <tomasz_wojnar@hotmail.com>
2024-01-02 14:51:36 +0100

    Add FindUnion data structure to SC2MapSegmentation/utils

---
+++ SC2MapSegmentation/utils/data_structures.py
@@ -0,0 +1,56 @@
+from typing import Hashable, Iterable, TypeVar
+
+T = TypeVar("T", bound=Hashable)
+
+
+class FindUnion:
+    """
+    Find-Union data structure
+
+    Args:
+        items (Iterable[T]): hashable items to be stored in the data structure
+    """
+
+    def __init__(self, items: Iterable[T]) -> None:
+        self.items = items
+        self.parents = {item: item for item in items}
+
+    def find(self, item: T) -> T:
+        """
+        Finds the parent of an item
+        
+        Args:
+            item (T): item to find the parent of
+            
+        Returns:
+            T: parent of the item
+        """
+        if self.parents[item] != item:
+            self.parents[item] = self.find(self.parents[item])
+        return self.parents[item]
+
+    def union(self, item1: T, item2: T) -> None:
+        """
+        Unions two items
+
+        Args:
+            item1 (T): first item to union
+            item2 (T): second item to union
+        """
+        self.parents[self.find(item1)] = self.find(item2)
+
+    def groups(self) -> list[set[T]]:
+        """
+        Returns the groups of items in the data structure
+
+        Returns:
+            list[set[T]]: groups of items in the data structure
+        """
+        groups = {}
+        for item in self.items:
+            parent = self.find(item)
+            if parent in groups:
+                groups[parent].add(item)
+            else:
+                groups[parent] = {item}
+        return list(groups.values())
